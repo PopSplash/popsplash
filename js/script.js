fb6805e0d0d59459d5a0c0b7a5e0746517e68be8
Nath <nath_m@msn.com>
2018-03-21 13:25:07 +0000

    Pushed from Dropbox

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@
 //fullpafe.js init
 $(document).ready(function() {
   $('#content').fullpage({
-    'verticalCentered': false,
+    'verticalCentered': true,
     'css3': true,
     'sectionsColor': ['#b0e8f5', '#b7d8f1', '#f6e2f0', '#e2d6f5', '#d8d5f0'],
     'anchors': ['showcase', 'about', 'folio', 'services', 'contact'],
-    'menu': '#header-nav-items',
+    'menu': '#navbar-items',
     'scrollingSpeed': 700,
+    'navigation': true,
     'navigationPosition': 'right',
     'navigationTooltips': ['PopSplash', 'About', 'Folio', 'Services', 'Contact'],
     'showActiveTooltip': true,
     'paddingTop': '100px',
     'paddingBottom': '100px',
-    'fixedElements': '#header',
     'slidesNavigation': true,
     'slidesNavPosition': 'bottom',
 
